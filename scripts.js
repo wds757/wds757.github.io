379149bdf5e095d40b7a491d2ae3b0dd31cb36c5
wds757 <124187428+wds757@users.noreply.github.com>
2023-04-16 08:42:03 -0400

    added new files
    to publish online

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+/*Menu toggle*/
+function menuToggle(x) {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
